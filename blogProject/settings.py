9f3f909e65f1328ffa5bfde42ece9d7c7ad355e4
latabinwal13 <lata.binwal@smartbuzz.net>
2018-07-25 15:03:39 +0530

    add static files to s3in django

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+
+try:
+	from .local_settings import *
+except ImportError as e:
+	pass
